888328f83f6fee4e8377b5b86edfad4d9f783942
Vrindavan Sanap <vrindavansanap@gmail.com>
2023-12-28 03:02:21 +0530

    -

--- app/crypto.js
+++ app/crypto.js
@@ -21,6 +21,7 @@ export function is_valid_base64(str) {
     }
   const base64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
   return base64Regex.test(str);
+
 }
 
 export function base64_to_binary(base64_string){
